55830d8d4713d7baad2511f21ebbcddce8383370
jordanSu <newjordansu1126@gmail.com>
2016-10-02 06:16:42 +0800

    loliList layout done

--- app/src/main/java/com/example/jj/jjloli/MainActivity.java
+++ app/src/main/java/com/example/jj/jjloli/MainActivity.java
@@ -40,15 +40,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         //mDrawerLayout.setScrimColor(Color.parseColor("#00FFFFFF"));
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
--- app/src/main/java/com/example/jj/jjloli/loliList.java
+++ app/src/main/java/com/example/jj/jjloli/loliList.java
@@ -1,10 +1,20 @@
 package com.example.jj.jjloli;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class loliList extends AppCompatActivity {
+    private ArrayList<FloatingActionButton> deletingButtons;
+    private ArrayList<FloatingActionButton> editingButtons;
+    private Button editButton;
+    private Boolean isEditing = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,9 +23,36 @@ public class loliList extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isEditing) {
+                    editButton.setText("Edit");
+                    //TODO
+                }
+                else {
+                    editButton.setText("Done");
+                    //TODO
+                }
+            }
+        });
 
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
 
+        deletingButtons.add((FloatingActionButton) findViewById(R.id.fab2));
+        deletingButtons.add((FloatingActionButton) findViewById(R.id.fab4));
+        deletingButtons.add((FloatingActionButton) findViewById(R.id.fab6));
+        deletingButtons.add((FloatingActionButton) findViewById(R.id.fab8));
+        deletingButtons.add((FloatingActionButton) findViewById(R.id.fab10));
+        deletingButtons.add((FloatingActionButton) findViewById(R.id.fab12));
 
+        editingButtons.add((FloatingActionButton) findViewById(R.id.fab1));
+        editingButtons.add((FloatingActionButton) findViewById(R.id.fab3));
+        editingButtons.add((FloatingActionButton) findViewById(R.id.fab5));
+        editingButtons.add((FloatingActionButton) findViewById(R.id.fab7));
+        editingButtons.add((FloatingActionButton) findViewById(R.id.fab9));
+        editingButtons.add((FloatingActionButton) findViewById(R.id.fab11));
     }
 
 }
